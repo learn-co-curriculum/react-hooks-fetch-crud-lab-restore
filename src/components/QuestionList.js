e02b74e12727f2b2569bbfb42dba7dde91d17930
ckjolhede <ckjolhede@gmail.com>
2024-02-28 22:50:06 -0700

    get function completed, all questions display on pageload

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react"; 
+import QuestionItem from "./QuestionItem";
+
+	const url =("http://localhost:4000/questions")
 
 function QuestionList() {
+	const [questions, setQuestions] = useState([]);
+
+	useEffect(() => {
+		fetch(url)
+      .then((res) => res.json())
+			.then((questions) => {
+				setQuestions(questions)
+			});
+	}, []);
+
+	const questionItems = questions.map((question) => <QuestionItem key={question.id} question={question} />);
+		
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
-    </section>
+			<ul>{questionItems}</ul>
+			
+		</section>
   );
 }
 
