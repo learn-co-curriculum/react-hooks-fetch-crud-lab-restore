3f167e86181e411c85195d78681351cb151762f4
ckjolhede <ckjolhede@gmail.com>
2024-02-29 05:16:16 -0700

    complete

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,28 +1,63 @@
-import React, { useEffect, useState } from "react"; 
+import React, { useEffect, useState } from "react";
 import QuestionItem from "./QuestionItem";
-
-	const url =("http://localhost:4000/questions/")
 
 function QuestionList() {
 	const [questions, setQuestions] = useState([]);
 
 	useEffect(() => {
-		fetch(url)
-      .then((res) => res.json())
+		fetch("http://localhost:4000/questions/")
+			.then((res) => res.json())
 			.then((questions) => {
-				setQuestions(questions)
+				setQuestions(questions);
 			});
 	}, []);
 
-	const questionItems = questions.map((question) => <QuestionItem key={question.id} question={question} />);
-		
-  return (
-    <section>
-      <h1>Quiz Questions</h1>
+	function handleDelete(id) {
+		fetch(`http://localhost:4000/questions/${id}`, {
+			method: "DELETE",
+		})
+			.then((res) => res.json())
+			.then(() => {
+				const remainingQuestions = questions.filter((question) => question.id !== id);
+				setQuestions(remainingQuestions);
+			});
+	}
+	
+	function handleAnswerChange(id, correctIndex) {
+		fetch(`http://localhost:4000/questions/${id}`, {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ correctIndex }),
+		})
+			.then((r) => r.json())
+			.then((updatedQuestion) => {
+				const updatedQuestions = questions.map((q) => {
+					if (q.id === updatedQuestion.id)
+						return updatedQuestion;
+					else
+						return q;
+				});
+				setQuestions(updatedQuestions);
+			});
+	}
+
+	const questionItems = questions.map((question) => (
+		<QuestionItem
+			key={question.id}
+			question={question}
+			deleteItem={handleDelete}
+			onAnswerChange={handleAnswerChange}
+		/>
+	));
+
+	return (
+		<section>
+			<h1>Quiz Questions</h1>
 			<ul>{questionItems}</ul>
-			
 		</section>
-  );
+	);
 }
 
 export default QuestionList;
