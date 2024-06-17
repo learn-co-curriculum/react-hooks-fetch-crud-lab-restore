f8fb2d7f4cce028ecfc33d5fc7ad40d16b9ac0fe
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-17 16:08:59 -0400

    Load questions upon page load

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionForm from "./QuestionForm";
+import QuestionItem from "./QuestionItem"
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((questionsData) => setQuestions(questionsData))
+  }, [])
+
+  console.log(questions)
+  const questionItems = questions.map((question) => {
+    return (
+      <QuestionItem
+        key={question.id}
+        question={question}
+      />
+    )
+  })
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionItems}</ul>
     </section>
   );
 }
