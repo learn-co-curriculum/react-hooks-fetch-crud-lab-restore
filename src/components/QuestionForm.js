587d9a99ef3c2fd972415fd25b433dd8b2557936
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-06-17 13:41:06 -0400

    Display all questions on load

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -19,7 +19,15 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((r) => r.json())
+      .then()
   }
 
   return (
